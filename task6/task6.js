b5df60f925831be39f719144ce79d1b1c15179ae
Dmitry <dmitry.d.komarov@gmail.com>
2020-09-10 15:51:23 +0300

    task 6

---
+++ task6/task6.js
@@ -0,0 +1,14 @@
+const changeImg = (e) => {
+    let elem = document.querySelector(".bigImage");
+    let source = e.target.getAttribute("src");
+    let newSource = source.replace("Small","Big");
+    elem.setAttribute("src", newSource);
+    //window.onerror = alert("error");
+}
+
+let images = document.querySelectorAll(".smallImage");
+console.log(images);
+for (i = 0; i < images.length; i++) {
+    console.log(images[i]);
+    images[i].addEventListener('click', changeImg);
+}
